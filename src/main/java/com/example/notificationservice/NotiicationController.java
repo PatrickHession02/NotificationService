9d2dbdfb46c00deb34dd5c3ab0fe0a4e3e359718
PATRICK HESSION <G00394619@gmit.ie>
2023-10-16 23:52:41 +0100

    Added NotificationController

--- src/main/java/com/example/notificationservice/NotiicationController.java
+++ src/main/java/com/example/notificationservice/NotiicationController.java
@@ -1,2 +1,11 @@
-package com.example.notificationservice;public class NotiicationController {
+package com.example.notificationservice;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationController {
+    @PostMapping("/sendNotification")
+    public void sendNotification(@RequestBody NotificationRequest request) {
+    }
 }
